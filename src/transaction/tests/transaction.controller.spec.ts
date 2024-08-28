fcc395aef689d38e621ceaf8d95239de7e477ea7
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-08-27 23:20:19 -0300

    feat: create unit tests for findOne and findByUserId

--- src/transaction/tests/transaction.controller.spec.ts
+++ src/transaction/tests/transaction.controller.spec.ts
@@ -26,6 +26,7 @@ describe('TransactionController', () => {
                         create: jest.fn(),
                         save: jest.fn(),
                         findOneBy: jest.fn(),
+                        findBy: jest.fn(),
                     },
                 },
                 {
@@ -89,6 +90,75 @@ describe('TransactionController', () => {
             });
             expect(transactionRepository.save).toHaveBeenCalledWith(newTransaction);
             expect(result).toEqual({ status: 'Transaction successful' });
+        });
+    });
+
+    describe('findOne', () => {
+        it('Should return a transaction by its ID', async () => {
+            const senderUser: User = { id: 'sender-uuid' } as User;
+            const receiverUser: User = { id: 'receiver-uuid' } as User;
+
+            const newTransaction: Transaction = {
+                id: '9d3b2c4d-6e2e-4b59-9d83-bd7f2f2c1b64',
+                amount: 5000,
+                description: 'Freelance service payment',
+                senderUser: senderUser,
+                receiverUser: receiverUser,
+            } as Transaction;
+
+            (transactionRepository.findOneBy as jest.Mock).mockResolvedValueOnce(newTransaction);
+
+            const result = await transactionController.findOne('e7196de0-a7ff-47fa-ae96-4cc76a732224');
+
+            expect(result.id).toEqual('9d3b2c4d-6e2e-4b59-9d83-bd7f2f2c1b64');
+            expect(result.amount).toEqual(5000);
+            expect(result.description).toEqual('Freelance service payment');
+            expect(result.senderUser.id).toEqual('sender-uuid');
+            expect(result.receiverUser.id).toEqual('receiver-uuid');
+        });
+    });
+
+    describe('findByUserId', () => {
+        it('should return transactions by user ID', async () => {
+            const senderUser: User = { id: 'sender-uuid' } as User;
+            const receiverUser: User = { id: 'receiver-uuid' } as User;
+
+            const newTransaction: Transaction[] = [
+                {
+                    id: '9d3b2c4d-6e2e-4b59-9d83-bd7f2f2c1b64',
+                    amount: 5000,
+                    description: 'Freelance service payment',
+                    senderUser: senderUser,
+                    receiverUser: receiverUser,
+                },
+                {
+                    id: '2f5a8e9c-3af1-42b6-9b75-dc8c62e3f1e7',
+                    amount: 12000,
+                    description: 'Travel expenses reimbursement',
+                    senderUser: senderUser,
+                    receiverUser: receiverUser,
+                },
+            ] as Transaction[];
+
+            (transactionRepository.findBy as jest.Mock).mockResolvedValueOnce(newTransaction);
+
+            const result = await transactionController.findByUserId('e7196de0-a7ff-47fa-ae96-4cc76a732224');
+
+            expect(result).toHaveLength(2);
+
+            expect(result[0].id).toEqual('9d3b2c4d-6e2e-4b59-9d83-bd7f2f2c1b64');
+            expect(result[1].id).toEqual('2f5a8e9c-3af1-42b6-9b75-dc8c62e3f1e7');
+
+            expect(result[0].amount).toEqual(5000);
+            expect(result[1].amount).toEqual(12000);
+
+            expect(result[0].description).toEqual('Freelance service payment');
+            expect(result[1].description).toEqual('Travel expenses reimbursement');
+
+            expect(result[0].senderUser).toEqual(senderUser);
+            expect(result[0].receiverUser).toEqual(receiverUser);
+            expect(result[1].senderUser).toEqual(senderUser);
+            expect(result[1].receiverUser).toEqual(receiverUser);
         });
     });
 });
