a1a5c92420978ad24802934162c91246dc9268d5
David Silva <davidrichardson1945@gmail.com>
2024-08-27 23:29:41 -0300

    build: adjust compose file to use an external network

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Transaction } from 'src/transaction/entities/transaction.entity';
 import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ synchronize: false })
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
